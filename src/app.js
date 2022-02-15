6c95f11365ade8b96a76acd734fe69fa152522e7
Adam <41493452+afl5082@users.noreply.github.com>
2022-02-15 18:54:43 -0500

    added validation tests, shape length, datatypes

--- src/app.js
+++ src/app.js
@@ -14,49 +14,100 @@
  * @return {boolean} A boolean indicating whether input matches expected{Cost,Year}
  */
  
- function checkArray(array){
-  return array.every(i => (typeof i == 'string'))
+function checkArrayKeys(object_keys){
+    return object_keys.every(i => (i == 'type' || i == 'year' || i == 'value' ))
 }
 
 function isValid (input, expectedCost, expectedOldestYear) {
+  
+    if (typeof(expectedCost) != 'number' || expectedCost < 0) {
+        console.log('Incorrect Input for ExpectedCost')
+        return false
+    }
+    
+    if (typeof(expectedOldestYear) != 'number' || expectedOldestYear < 0 || expectedOldestYear > 2023) {
+        console.log('Incorrect Input for expectedOldestYear')
+        return false
+    }
 
-  if (typeof(expectedCost) != 'number' || expectedCost < 0) {
-      console.log('Incorrect Input for Cost')
-      return false
+    //validating input is an array 
+    if (input.constructor.name != 'Array') {
+        console.log('Incorrect Input Type for Vehicle List')
+        return false
+    }
+    
+
+    let sum_cost = 0 
+
+
+    //loop over list of Vehicle objects
+    for (i = 0; i < input.length ; i++){
+
+        sum_cost += input[i].value
+
+        let shape_length = Object.keys(input[i]).length
+        let shape_keys = Object.keys(input[i])
+
+        //testing shape(key attributes) and length of object Vehicle 
+        if (shape_length != 3 || checkArrayKeys(shape_keys) == false) {
+            console.log('Incorrect Keys in Vehicle Object')
+            return false
+        }
+
+        //testing if year is less than expected oldest year
+        //if expected 'oldest' year is 1990, should not have cars with years of 1989
+        if (input[i].year < expectedOldestYear){
+            console.log('Vehcile Year is too old')
+            return false
+        }
+
+        if (typeof(input[i].type) != 'string'){
+            console.log('Incorrect data type for Type in Vehicle Object')
+            return false
+        }
+
+        if (typeof(input[i].year) != 'number'){
+              //if time, add seperate check for sensible year
+            console.log('Incorrect data type for Year in Vehicle Object')
+            return false
+        }
+
+        if (typeof(input[i].value) != 'number'){
+            console.log('Incorrect data type for value in Vehicle Object')
+            return false
+        }
+
+    }
+
+    if (sum_cost != expectedCost){
+        console.log('Incorrect Sum of all Vehicles compared to Expected Cost')
+        return false
+    }
+   
+    return true
   }
   
-  if (typeof(expectedOldestYear) != 'number' || expectedOldestYear < 0 || expectedOldestYear > 2023) {
-      console.log('Incorrect Input for Year')
-      return false
+  /**
+   * Prompt: Implement a few tests to validate that your function works as expected
+   */
+  function testIsValid() {
+    let validate_test1  = isValid(
+        
+        [{
+         type: 'Truck',
+         year: 1980,
+         value: 1500
+        }],
+        1500,
+        1980
+        
+        )
+    console.log(validate_test1)
   }
-  if (input.constructor.name != 'Array' || checkArray(input) == false) {
-      console.log('Incorrect Input for Vehicle')
-      return false
-  }
- 
-  return true
-}
-
-/**
- * Prompt: Implement a few tests to validate that your function works as expected
- */
-function testIsValid() {
-  let validate_test1  = isValid(['Truck','Trailer','Tractor'],45,2500)
-  console.log(validate_test1)
-
-  let validate_test2  = isValid('a string',1500,'a string')
-  console.log(validate_test2)
-
-  let validate_test3  = isValid([50,70,50],1500,1980)
-  console.log(validate_test3)
-
-  let validate_test4  = isValid(['Truck','Trailer','Tractor'],1500,1980)
-  console.log(validate_test4)
-}
-
-
-testIsValid()
-
- /**
- * If more time, would implement better logging for incorrect values 
- */
+  
+  
+  testIsValid()
+  
+   /**
+   * If more time, would implement better logging for incorrect values 
+   */
