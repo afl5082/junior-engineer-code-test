1c4a561c08fbcca7d579e6dad72d19a124bba509
adam <adam.lacaria@gmail.com>
2022-02-15 15:20:24 -0500

    view_count added, date fixed, js functions

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ import datetime
 from django.db import models
 
 def auto_now():
-    return datetime.datetime.now() - datetime.timedelta(days=1)
+    return datetime.datetime.now() #- datetime.timedelta(days=1)
 
 class Post(models.Model):
     title = models.CharField(max_length=128)
@@ -10,6 +10,7 @@ class Post(models.Model):
     posted_by = models.CharField(max_length=50, default="root")
     posted_at = models.DateTimeField(default=auto_now)
     edited_at = models.DateTimeField(auto_now=True)
+    view_count = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
--- src/app.js
+++ src/app.js
@@ -13,13 +13,50 @@
  * @param {number} expectedOldestYear The expected minimum year of all vehicles passed in
  * @return {boolean} A boolean indicating whether input matches expected{Cost,Year}
  */
+ 
+ function checkArray(array){
+  return array.every(i => (typeof i == 'string'))
+}
+
 function isValid (input, expectedCost, expectedOldestYear) {
-  // Your code here
+
+  if (typeof(expectedCost) != 'number' || expectedCost < 0) {
+      console.log('Incorrect Input for Cost')
+      return false
+  }
+  
+  if (typeof(expectedOldestYear) != 'number' || expectedOldestYear < 0 || expectedOldestYear > 2023) {
+      console.log('Incorrect Input for Year')
+      return false
+  }
+  if (input.constructor.name != 'Array' || checkArray(input) == false) {
+      console.log('Incorrect Input for Vehicle')
+      return false
+  }
+ 
+  return true
 }
 
 /**
  * Prompt: Implement a few tests to validate that your function works as expected
  */
-function testIsValid () {
-  // Your code here
+function testIsValid() {
+  let validate_test1  = isValid(['Truck','Trailer','Tractor'],45,2500)
+  console.log(validate_test1)
+
+  let validate_test2  = isValid('a string',1500,'a string')
+  console.log(validate_test2)
+
+  let validate_test3  = isValid([50,70,50],1500,1980)
+  console.log(validate_test3)
+
+  let validate_test4  = isValid(['Truck','Trailer','Tractor'],1500,1980)
+  console.log(validate_test4)
 }
+
+
+testIsValid()
+
+ /**
+ * If more time, would implement better logging for incorrect values 
+ */
