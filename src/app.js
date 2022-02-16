3a910e5a85070def75ea69ae85f47788574d2317
Adam <41493452+afl5082@users.noreply.github.com>
2022-02-16 09:43:21 -0500

    additional test inputs

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@
  * @return {boolean} A boolean indicating whether input matches expected{Cost,Year}
  */
  
+//check keys contain type, year, value
 function checkArrayKeys(object_keys){
     return object_keys.every(i => (i == 'type' || i == 'year' || i == 'value' ))
 }
@@ -25,7 +26,7 @@ function isValid (input, expectedCost, expectedOldestYear) {
         return false
     }
     
-    if (typeof(expectedOldestYear) != 'number' || expectedOldestYear < 0 || expectedOldestYear > 2023) {
+    if (typeof(expectedOldestYear) != 'number' || expectedOldestYear < 1850 || expectedOldestYear > 2023) {
         console.log('Incorrect Input for expectedOldestYear')
         return false
     }
@@ -66,14 +67,13 @@ function isValid (input, expectedCost, expectedOldestYear) {
             return false
         }
 
-        if (typeof(input[i].year) != 'number'){
-              //if time, add seperate check for sensible year
-            console.log('Incorrect data type for Year in Vehicle Object')
+        if (typeof(input[i].year) != 'number' || input[i].year > 2023 || input[i].year < 1850 ){
+            console.log('Incorrect input for Year in Vehicle Object')
             return false
         }
 
-        if (typeof(input[i].value) != 'number'){
-            console.log('Incorrect data type for value in Vehicle Object')
+        if (typeof(input[i].value) != 'number' || input[i].value < 0 ){
+            console.log('Incorrect input for Value in Vehicle Object')
             return false
         }
 
@@ -90,24 +90,137 @@ function isValid (input, expectedCost, expectedOldestYear) {
   /**
    * Prompt: Implement a few tests to validate that your function works as expected
    */
+  
+  //
   function testIsValid() {
+    
+    
+    //Valid entries -- return true
     let validate_test1  = isValid(
-        
-        [{
-         type: 'Truck',
-         year: 1980,
-         value: 1500
-        }],
-        1500,
+        [
+            {
+            type: 'Truck',
+            year: 1980,
+            value: 1500
+            },
+            {
+            type: 'Trailer',
+            year: 2000,
+            value: 1500
+            },
+        ],
+        3000,
         1980
         
         )
     console.log(validate_test1)
-  }
+
+    //expectedCost is not equal to sum -- return false    
+    let validate_test2  = isValid(
+        [
+            {
+            type: 'Truck',
+            year: 1980,
+            value: 1500
+            },
+            {
+            type: 'Trailer',
+            year: 2000,
+            value: 1600
+            },
+        ],
+        3000,
+        1980
+        
+        )
+    console.log(validate_test2)
+    
+    //Vehicle is too old -- return false
+    let validate_test3 = isValid(
+    [
+        {
+        type: 'Truck',
+        year: 1975,
+        value: 1500
+       }
+    ],
+    1500,
+    1980
+       
+       )
+   console.log(validate_test3)
+
+
+    //Vehicle type, datatype is a number -- return false
+    let validate_test4 = isValid(
+    [
+        {
+        type: 50,
+        year: 1991,
+        value: 1500
+       }
+    ],
+    1500,
+    1980
+       
+       )
+   console.log(validate_test4)
+
+    //incorrect, extra key value (color) in Vehicle object -- return false
+    let validate_test5 = isValid(
+    [
+        {
+        type: 50,
+        year: 1991,
+        value: 1500,
+        color: 'red'
+       }
+    ],
+    1500,
+    1980
+       
+       )
+   console.log(validate_test5)
+
+
+    //unsensible year (in future) in Vehicle object -- return false
+    let validate_test6 = isValid(
+    [
+        {
+        type: 'Tractor',
+        year: 2050,
+        value: 1500
+       }
+    ],
+    1500,
+    1980
+       
+       )
+   console.log(validate_test6)
+
+
+    //unsensible negative value in Vehicle object -- return false
+    let validate_test7 = isValid(
+    [
+        {
+        type: 'Tractor',
+        year: 2011,
+        value: -1500
+       }
+    ],
+    1500,
+    1980
+       
+       )
+   console.log(validate_test7)
+  
+
+
+}
   
   
-  testIsValid()
+testIsValid()
   
-   /**
-   * If more time, would implement better logging for incorrect values 
-   */
+/**
+* If more time, would implement better logging for incorrect values 
+ */
